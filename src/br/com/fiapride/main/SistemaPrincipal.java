3fe8f0b487b50c3b0f81692d7398737493343d8c
mimasc <mirella.mascarenhas1@gmail.com>
2026-03-09 09:44:56 -0300

    Add comments and improve readability in main class

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -3,18 +3,21 @@ package br.com.fiapride.main;
 
 import br.com.fiapride.model.Veiculos;
 
+// classe principal que executa o projeto
 public class SistemaPrincipal {
 
 	public static void main(String[] args) {
-		
+		// criando um objeto da classe Veiculos
 		Veiculos v1 = new Veiculos();
+		// definições
 			v1.setIndividuo("Carlos");
 			v1.setPlaca("ABC-1234");
 			v1.setGasolina(10);
 			
 			v1.adicionarGasolina(50);
 			v1.gastaGasolina(100);
-			
+
+		// printando as informações
 			System.out.println("Dono: Carlos | Placa: " + v1.getPlaca() + "| Gasolina: " + v1.getGasolina());
 
 	}
